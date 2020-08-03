d6b25f0b00ccd3c938907d31b67cde03e4f5d07e
reshetnikd <dmitry.reshetnik@icloud.com>
2020-08-03 19:42:15 +0300

    Add Core Data

--- GitHubCommits/ViewController.swift
+++ GitHubCommits/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UITableViewController {
+    var container: NSPersistentContainer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        container = NSPersistentContainer(name: "GitHub")
+        
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error {
+                print("Unresolved error \(error)")
+            }
+        }
+    }
+    
+    func saveContext() {
+        if container.viewContext.hasChanges {
+            do {
+                try container.viewContext.save()
+            } catch {
+                print("An error occured while saving: \(error)")
+            }
+        }
     }
 
 
