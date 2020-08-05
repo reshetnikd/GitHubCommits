653a94c3094e3360df022996242691fae6cd8a78
reshetnikd <dmitry.reshetnik@icloud.com>
2020-08-05 16:15:48 +0300

    Add NSFetchRequest to load data

--- GitHubCommits/ViewController.swift
+++ GitHubCommits/ViewController.swift
@@ -11,6 +11,7 @@ import CoreData
 
 class ViewController: UITableViewController {
     var container: NSPersistentContainer!
+    var commits = [Commit]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,25 @@ class ViewController: UITableViewController {
         }
         
         performSelector(inBackground: #selector(fetchCommits), with: nil)
+        loadSavedData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commits.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Commit", for: indexPath)
+        
+        let commit = commits[indexPath.row]
+        cell.textLabel?.text = commit.message
+        cell.detailTextLabel?.text = commit.date.description
+        
+        return cell
     }
     
     @objc func fetchCommits() {
@@ -43,6 +63,7 @@ class ViewController: UITableViewController {
                 }
                 
                 self.saveContext()
+                self.loadSavedData()
             }
         }
     }
@@ -64,6 +85,20 @@ class ViewController: UITableViewController {
         
         let formatter = ISO8601DateFormatter()
         commit.date = formatter.date(from: json["commit"]["commiter"]["date"].stringValue) ?? Date()
+    }
+    
+    func loadSavedData() {
+        let request = Commit.createFetchRequest()
+        let sort = NSSortDescriptor(key: "date", ascending: false)
+        request.sortDescriptors = [sort]
+        
+        do {
+            commits = try container.viewContext.fetch(request)
+            print("Got \(commits.count) commits")
+            tableView.reloadData()
+        } catch {
+            print("Fetch failed")
+        }
     }
 
 
